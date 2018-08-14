dac680cae38750e83f1165a8359d0eb58a43d263
17010233 <17010233@myrp.edu.sg>
2018-08-14 14:35:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a17010233/senderapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a17010233.senderapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent ("com.example.broadcast.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
